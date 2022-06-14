6a268263750a912d47586ec82bbbb4e0a6fc0945
Felipe Branco <43156537+FelpsBR97@users.noreply.github.com>
2022-06-13 22:29:05 -0300

    A little change

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-function fetchApiData() {
-    fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-    .then(response => response.json())
-    .then(data => {
-        const list = document.querySelector('ul')
-        data.data.map(item => {
-            const li = document.createElement('li')
-            li.setAttribute('id', item.id)
-            li.innerHTML = item.name
-            list.appendChild(li)
-        })
+fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+.then(response => response.json())
+.then(data => {
+    const list = document.querySelector('ul')
+    data.data.map(item => {
+        const li = document.createElement('li')
+        li.setAttribute('id', item.id)
+        li.innerHTML = `${item.name} <br>${item.type}`
+        list.appendChild(li)
     })
-}
+})
