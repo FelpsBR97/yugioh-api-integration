1338926c59ac0ab876126e0594496d4209afabb6
Felipe Branco <43156537+FelpsBR97@users.noreply.github.com>
2022-08-15 20:07:54 -0300

    Starting to filter cards
    Here I implement a function to filter list elements using input field.
    I also modified the way I was calling the API and removed the search button to make searching easier.

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-.then(response => response.json())
-.then(data => {
-    const list = document.querySelector('ul')
-    data.data.map(item => {
-        const li = document.createElement('li')
-        li.setAttribute('id', item.id)
-        li.innerHTML = `${item.name} <br>${item.type}`
-        list.appendChild(li)
+const api_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+async function getCards(){
+  fetch(api_url).then(response => response.json())
+  .then(data => {
+      const list = document.querySelector('ul')
+      data.data.map(item => {
+         const li = document.createElement('li')
+          li.setAttribute('id', item.id)
+          li.innerHTML = `<a>${item.name} <br>${item.type}<br>ATK:${item.atk} DEF:${item.def}</a>`
+          list.appendChild(li)
+      })
     })
-})
+}
+getCards()
+
+function searchCard() {
+    var input, filter, ul, li, a, i, txtValue
+    input = document.getElementById('userInput')
+    filter = input.value.toUpperCase()
+    ul = document.querySelector('ul')
+    li = ul.getElementsByTagName('li')
+
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0]
+      txtValue = a.textContent || a.innerText
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = ""
+      } else {
+        li[i].style.display = "none"
+      }
+    }
+  }
